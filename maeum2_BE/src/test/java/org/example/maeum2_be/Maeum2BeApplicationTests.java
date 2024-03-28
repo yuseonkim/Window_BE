e3ff2926cd2468352c61624188184a5b63816ab0
chaendaya <jini971@naver.com>
2024-03-28 14:31:17 +0900

    conf : 프로젝트 환경설정
    스프링부트 환경설정

---
+++ maeum2_BE/src/test/java/org/example/maeum2_be/Maeum2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.maeum2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Maeum2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
