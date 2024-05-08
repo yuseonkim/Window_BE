d1fa6254bc789f6dce2cf91450b1325c7336407d
chaendaya <jini971@naver.com>
2024-05-08 14:48:11 +0900

    feat : GPT code error 해결

--- maeum2_BE/src/main/java/org/example/maeum2_be/service/gpt/GPTService.java
+++ maeum2_BE/src/main/java/org/example/maeum2_be/service/gpt/GPTService.java
@@ -1,6 +1,8 @@
 package org.example.maeum2_be.service.gpt;
 
 import lombok.RequiredArgsConstructor;
+import org.example.maeum2_be.dto.GPTRequestDTO;
+import org.example.maeum2_be.dto.GPTResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
