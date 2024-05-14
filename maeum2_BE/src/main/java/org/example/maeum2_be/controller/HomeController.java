566f50235810962671e40f437beb4fb779665698
yuseonkim <yusun715@icloud.com>
2024-05-15 04:57:29 +0900

    feat : 마이페이지 컨트롤러 및 응답DTO 작성

--- maeum2_BE/src/main/java/org/example/maeum2_be/controller/HomeController.java
+++ maeum2_BE/src/main/java/org/example/maeum2_be/controller/HomeController.java
@@ -1,21 +1,46 @@
 package org.example.maeum2_be.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.example.maeum2_be._core.ApiResponse;
 import org.example.maeum2_be._core.ApiResponseGenerator;
 import org.example.maeum2_be.dto.HomeDTO;
+import org.example.maeum2_be.dto.myPage.GuardianInformation;
+import org.example.maeum2_be.dto.myPage.KidInformation;
+import org.example.maeum2_be.dto.myPage.MyPageDTO;
+import org.example.maeum2_be.entity.domain.Member;
 import org.example.maeum2_be.entity.domain.PrincipalDetails;
+import org.example.maeum2_be.repository.MemberRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/api/home")
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/api/myPage")
     public ApiResponse<?> home(@AuthenticationPrincipal PrincipalDetails principalDetails){
-        String childFirstName = principalDetails.getUsername();
-        String message ="일단 테스트";
-        return ApiResponseGenerator.success(new HomeDTO(childFirstName,message), HttpStatus.OK);
+        Member member = memberRepository.findByMemberId(principalDetails.getMemberId());
+        List<KidInformation> kidInformations = new ArrayList<>();
+        kidInformations.add(new KidInformation("캐릭터이름",member.getAiName()));
+        kidInformations.add(new KidInformation("성별",member.getChildLastName()));
+        kidInformations.add(new KidInformation("이름",member.getChildFirstName()));
+        kidInformations.add(new KidInformation("생년월일",member.getChildBirth().toString()));
+        kidInformations.add(new KidInformation("성",member.getChildGender()));
+
+        List<GuardianInformation> guardianInformations = new ArrayList<>();
+        guardianInformations.add(new GuardianInformation("이메일", member.getEmail()));
+        guardianInformations.add(new GuardianInformation("연락처", member.getPhoneNumber()));
+
+        MyPageDTO myPageDTO = new MyPageDTO(kidInformations,guardianInformations);
+        return ApiResponseGenerator.success(myPageDTO, HttpStatus.OK);
     }
+
+
 }
---
+++ maeum2_BE/src/main/java/org/example/maeum2_be/dto/myPage/GuardianInformation.java
@@ -0,0 +1,13 @@
+package org.example.maeum2_be.dto.myPage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class GuardianInformation {
+    private String key;
+    private String value;
+}
