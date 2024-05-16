c0fb3d0ca6d31c208a29491a5c6016a2f1d4b29e
yuseonkim <yusun715@icloud.com>
2024-05-16 17:27:06 +0900

    feat : GPT 관련 DTO 타입 수정

--- maeum2_BE/src/main/java/org/example/maeum2_be/controller/GPTController.java
+++ maeum2_BE/src/main/java/org/example/maeum2_be/controller/GPTController.java
@@ -1,16 +1,20 @@
 package org.example.maeum2_be.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.maeum2_be.dto.GPTRequestDTO;
-import org.example.maeum2_be.dto.GPTResponseDTO;
-import org.example.maeum2_be.dto.MessageDTO;
-import org.example.maeum2_be.dto.UserInputDTO;
+import org.example.maeum2_be._core.ApiResponse;
+import org.example.maeum2_be._core.ApiResponseGenerator;
+import org.example.maeum2_be.dto.*;
 import org.example.maeum2_be.service.gpt.GPTService;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,20 +22,25 @@ public class GPTController {
     private final GPTService gptService;
 
     @PostMapping("/api/main/gpt")
-    public String processGPT(@RequestBody UserInputDTO userInputDTO) {
+    public ApiResponse<?> processGPT(@RequestBody UserInputDTO userInputDTO) {
 
         // 클라이언트로부터 받은 사용자 입력 설정
+        List<MessageDTO> messageDTOList = new ArrayList<>();
         MessageDTO userMessage = new MessageDTO();
         userMessage.setRole("user");
         userMessage.setContent(userInputDTO.getUserInput());
 
+        messageDTOList.add(userMessage);
         // GPT 처리
         GPTRequestDTO requestDTO = new GPTRequestDTO();
-        requestDTO.setMessages(userMessage);
+        requestDTO.setMessages(messageDTOList);
         requestDTO.setModel("gpt-3.5-turbo");
 
         GPTResponseDTO gptResponse = gptService.getResponse(requestDTO);
-        String text = gptResponse.getChoices().getMessage().getContent();
+        List<String> textList = new ArrayList<>();
+        for(ChoiceDTO text : gptResponse.getChoices()){
+            textList.add(text.getMessage().getContent());
+        }
 
         // JSON 처리
 
@@ -40,6 +49,6 @@ public class GPTController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType("application/json"));
 
-        return text;
+        return ApiResponseGenerator.success(new TextResponseDTO(textList), HttpStatus.OK);
     }
 }
--- maeum2_BE/src/main/java/org/example/maeum2_be/dto/ChoiceDTO.java
+++ maeum2_BE/src/main/java/org/example/maeum2_be/dto/ChoiceDTO.java
@@ -1,22 +1,11 @@
 package org.example.maeum2_be.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ChoiceDTO {
     private int index;
     private MessageDTO message;
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public MessageDTO getMessage() {
-        return message;
-    }
-
-    public void setMessage(MessageDTO message) {
-        this.message = message;
-    }
 }
